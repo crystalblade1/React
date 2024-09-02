a44af9b2b40b76009c8ddd1d1cbbb192ac96f8c3
crystalblade1 <34961129+crystalblade1@users.noreply.github.com>
2024-09-02 11:52:56 +0400

    Delete first-react-app directory

--- first-react-app/src/components/Ecommerce/Thumb-jquery.js
+++
@@ -1,9 +0,0 @@
-
-
-<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
-$(document).ready(function(){
-    $('.thumb a').click(function(e){
-      e.preventDefault();
-      $('.imgbox img').attr("src", $(this).attr("href"));
-    });
-  });
